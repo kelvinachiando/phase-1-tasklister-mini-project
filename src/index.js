444d0f30e0930a79cbbac54fb68d4a74a83db1a9
kelvinachiando <kelvinostin22gmail.com>
2022-06-13 22:21:12 +0300

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form")
+  form.addEventListener("submit", e => {
+    e.preventDefault()
+    buildToDo (e.target ["new-task-descritption"].value)
+    form.reset ()
+  })
 });
+// Stretch Deliverables
